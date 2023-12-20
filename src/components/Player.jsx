f9980ecc9c2fdb4cb1f2352eff4c923f9e3a34aa
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-12-20 15:26:53 -0300

    make Player button functional

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    //if the new state depends on the previous state, this shouldnt be used
+    //setIsEditing(!isEditing);
+    //because react is scheduling this update and in some scenarios it may break
+    //in this case here this delay will be like 2ms so wouldnt really matter
+
+    //instead its better to pass a function so it will always get the currently value
+    //the other way may give the old value
+    setIsEditing((editing) => !editing);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && <input type="text" required value={name} />}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
