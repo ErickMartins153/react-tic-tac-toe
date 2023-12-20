ecb64d945cb11da3e2ac09b8faedb1a2bcbbca56
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-12-20 15:55:16 -0300

    make input tag work

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -14,11 +15,24 @@ export default function Player({ name, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
+  function handleChange(e) {
+    //we're getting the value from the input and updating it also in the input
+    //this is called two-way-binding, we refeed the value to the place where we got it
+    setPlayerName(e.target.value);
+  }
+
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input type="text" required value={name} />}
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={(e) => handleChange(e)}
+          />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
 
