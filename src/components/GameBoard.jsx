f13f8fde7052db564456021172a74b7fa105adc4
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-12-21 12:13:01 -0300

    add gameboard component and basic functions

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      //doing it like:
+      /*
+      prevGameBoard[rowIndex][colIndex] = "X";
+      return prevGameBoard; */
+      //its not recommended, objects and arrays are reference values
+      //and should never be mutate directly, its better to create a copy of it
+      //and then change the copy and return it.
+      //if we did it directly we'd be updating the old value in memory imediately
+      //before even the react schedule thing happened properly, so this could lead to
+      //weird side effects and bugs
+
+      //we had to use map to also spread the innerArrays, or it would be the reference
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
