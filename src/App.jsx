e68c4a620e52d6794491b7e09896d215d23cbf38
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-12-22 09:16:51 -0300

    get rid of activePlayer state, derive it from gameTurns

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,36 @@ import Log from "./components/Log";
 //to keep track of, in this case the App component has both Players and Gameboard inside of it
 //so the app component will manage the state to keep track of whose turn is
 
+//this helper function is outside the component because it wont need access to any state or data related
+//to that component, and it also shouldnt be recreated when the component rerenders
+function deriveActivePlayer(gameTurns) {
+  /* ok since we moved it to a function ignore the following comments */
+
+  //even tho the code is almost the same as the one in handleSelectSquare we cant remove it from there
+  //because we need to derive the state from the old state, and here we're getting from the current
+
+  let currentPlayer = "X"; //so now we're deriving the symbol from gameTurns
+  //instead of getting it from the setGameTurns arrow function way, we can get the currently
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  //since we're using gameTurns state, this component is already updated by the square click
+  //so we can remove this activePlayer and change the logic, getting it from gameTurns
+  //const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(row, column) {
-    setActivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
-    );
+    //setActivePlayer((currentActivePlayer) =>currentActivePlayer === "X" ? "O" : "X");
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        //so instead of trusting that activePlayer is updated(since it comes from another state)
-        //we can take advantage that the game always starts from X and then checking the last object
-        //if the last object data has an X, this one needs to be an O and vice-versa
-        currentPlayer = "O";
-      }
+      //with this following lines we're already getting the current player based on the last turn
+      //so we really dont need activePlayer state
+      let currentPlayer = deriveActivePlayer(prevTurns);
       //instead of doing player: activePlayer we should add a let inside this function
       const updatedTurns = [
         { square: { row: row, column: column }, player: currentPlayer },
